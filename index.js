bbd29b3f27a492608644828b838ef9b3ec0a31c9
BobbyCharms <robertcharming@Gmail.com>
2023-03-19 18:18:10 -0400

    was able to generate svg file

--- index.js
+++ index.js
@@ -1,2 +1,66 @@
 const fs = require('fs');
-const inquier = require('inquirer')
+const inquirer = require('inquirer')
+const {Triangle, Circle, Square} = require('./lib/shapes');
+
+const colorsOptions =
+    ["blue", "black", "white", "maroon", "red", "purple", "green", "yellow", "navy",]
+
+const questions = [
+    {
+        type: "list",
+        message: "Would you like a Triangle, Square, or Circle logo?",
+        name: "shape",
+        choices: ["Triangle", "Circle", "Square"],
+        default: "Triangle"
+    },
+    {
+        type: "input",
+        message: "What color would you like for your logo?",
+        name: "bkgColor",
+        validate: function(input) {
+            if (colorsOptions.includes(input.toLowerCase())) {
+                return true
+            } else {
+                return " Please select from blue, black, white, ,maroon, red, purple, green, yellow, or navy."
+            }
+        }
+    },
+    {
+        type: "input",
+        message: "Please type in 3 characters (max) for your logo.",
+        name: "text",
+        validate: function(input) {
+            if (input.length < 4 && input.length > 0) {
+                return true
+            } else {
+                return "Your logo can only be between 1-3 characters."
+            }
+        }
+    },
+]
+
+const makeFile = function(fileName, data) {
+    fs.writeFile(fileName, data, "utf8", (err) => err ? console.log(err) : console.log("You've created a logo.svg file." ));
+}
+
+function generateLogo(answers) {
+    shape = `${answers.shape}`
+    bkgColor = `${answers.bkgColor}`
+    text = `${answers.text}`
+    let logo;
+        if (shape === "Triangle") {
+            logo = new Triangle(bkgColor, text)
+        } else if (shape === "Circle") {
+            logo = new Circle(bkgColor, text)
+        } else {
+            logo = new Square(bkgColor, text)
+        }
+    return logo.createShape();
+}
+
+inquirer    
+    .prompt([...questions])
+    .then((answers) => {
+        const logo = generateLogo(answers)
+        makeFile("logo.svg", logo)
+    });
